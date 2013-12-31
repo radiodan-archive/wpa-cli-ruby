604e53d9850b222cde28f2ec21479d55d1911cfb
Jordan S <jschaenzle@gmail.com>
2013-12-31 10:06:55 -0500

    Update wpa_cli_wrapper.rb
    Fixed execute to allow escaped quotes

--- lib/wpa_cli_ruby/wpa_cli_wrapper.rb
+++ lib/wpa_cli_ruby/wpa_cli_wrapper.rb
@@ -1,47 +1,47 @@
 module WpaCliRuby
   class WpaCliWrapper
-    def execute(cmd)
-      %x[#{cmd}]
+    #def execute(cmd)
+    #  %x[#{cmd}]
+    #end
+    
+    def execute(*args)
+      IO.popen("wpa_cli", "r", *args) do |io|
+        io.read
+      end
     end
-
+    
     def self.available?
       system("which wpa_cli > /dev/null 2>&1")
     end
 
     def scan
-      cmd = "wpa_cli scan"
-      execute(cmd)
+      execute("scan")
     end
 
     def scan_results
-      cmd = "wpa_cli scan_results"
-      execute(cmd)
+      execute("scan_results")
     end
 
     def add_network
-      cmd = "wpa_cli add_network"
-      execute(cmd)
+      execute("add_network")
     end
 
     def set_network(network_id, key, value)
       value = "\"#{value}\"" unless value.is_a? Symbol
-      cmd = "wpa_cli set_network #{network_id} #{key} '#{value}'"
-      execute(cmd)
+      execute("set_network", network_id, key, value)
     end
 
     def get_network(network_id, key)
-      cmd = "wpa_cli get_network #{network_id} #{key}"
-      execute(cmd)
+
+      execute("get_network", network_id, key)
     end
 
     def enable_network(network_id)
-      cmd = "wpa_cli enable_network #{network_id}"
-      execute(cmd)
+      execute("enable_network", network_id)
     end
 
     def save_config
-      cmd = "wpa_cli save_config"
-      execute(cmd)
+      execute("save_config")
     end
   end
 end
